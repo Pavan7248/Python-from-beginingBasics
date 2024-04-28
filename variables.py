d446a281f6a4f4731f8252ab4ac9c05cc01b7e9f
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-04-28 14:43:19 +0000

    Added variables notes

---
+++ variables.py
@@ -0,0 +1,15 @@
+#variable is a container for storing data types.
+# variables can hold value of any data type like string, integer, float, boolean, list, tuple, dictionary, etc.
+# variable is created when we assign a value to it.
+
+x = 10
+print(x)
+y = "Pavan"
+z = 2.13
+print(y)
+print(z)
+
+# can also change the value of a variable by reassigning it to a new value.
+
+x = 20  
+print(x)
