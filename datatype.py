c164cd7a669142baf1f70cc62e13494caf2082b4
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-04-28 14:49:51 +0000

    Added Data types

---
+++ datatype.py
@@ -0,0 +1,21 @@
+# Python has several built-in data types, including strings, integers, floats, and booleans.
+
+# Strings are a sequence of characters enclosed in single or double quotes.
+print("Hello World")
+
+# Numbers are integers (whole numbers) and floats (decimal numbers).
+print(10)
+print(2.13)
+# Booleans are either True or False.
+print(True)
+print(False)
+# Lists are a collection of items enclosed in square brackets separated by comma.
+print([1, 2, 3, 4, 5])
+# Tuples are a collection of items enclosed in parentheses separated by comma.
+print((1, 2, 3, 4, 5))
+# Dictionaries are a collection of key-value pairs enclosed in curly braces.
+print({"name": "Pavan", "age": 25})
+# None is a special value that represents the absence of a value.
+print(None)
+
+
