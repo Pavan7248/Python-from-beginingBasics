a557bf3e48d132fe287f8f52cec6455190f4261c
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-04-28 14:25:09 +0000

    Initial commit

---
+++ Intro.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print(20)
+print("My name is Pavan", 10)
+print(8 + 9)
---
+++ comments.py
@@ -0,0 +1,9 @@
+# Comments are the part of the program file which a programmer does not want to execute.
+# Comments are used to explain the code and make it more readable.
+# To add a comment, we use the # symbol.
+# Escape sequence are used to insert special characters in a string.
+# print("Hello World")
+# print(20)
+# ctrl + / is used to comment or uncomment the selected lines.
+# alt+down arrow is used to move the line down.
+#to add a special charater in a string, we use the escape sequence i.e, \n, \t, \", \', \\
