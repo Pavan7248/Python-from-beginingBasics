14857665c114412f76fa909990f2d326ef13030d
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-02 06:06:12 +0000

    Added f-string

--- fstring.py
+++ fstring.py
@@ -11,3 +11,32 @@ print(txt)
 price = 59
 txt = f"The price is {price} dollars"
 print(txt)
+
+#A modifier can be used to format the output of the f-string.
+#A modifier is included by adding a colon : followed by a 
+ #legal formatting type, like .2f which means fixed point number with 2 decimals:
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+#perform operations in f-string
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+#A placeholder is used to define the type of data that will be passed into the f-string.
+price = 59
+tax = 0.25
+txt = f"The price is {price + (price * tax)} dollars"
+print(txt)
+
+#can perform if-else in f-string
+price = 49
+txt = f"It is very {'Expensive' if price>50 else 'Cheap'}"
+
+print(txt)
+
+#can execute functions in f-string
+fruit = "apples"
+txt = f"I love {fruit.upper()}"
+print(txt)
