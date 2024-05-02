874be980836d51f265b4f23e4edec3868bb47d46
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-02 05:50:45 +0000

    F-STRING

---
+++ fstring.py
@@ -0,0 +1,13 @@
+#F-string allows you to embed expressions inside string literals, using a minimal syntax
+#to specify the stringas a f-string ,simply prefix the string with the letter f.
+
+txt = f"The price is 49 dollars"
+print(txt)
+# Modifiers and Placeholders
+# The f-string allows you to embed expressions inside 
+ # string literals, using a minimal syntax to specify the 
+ #string as a f-string, simply prefix the string with the letter f.
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
