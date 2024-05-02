323c7701d012b402cb98c0834233cd67eb66cebf
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-02 05:41:03 +0000

    added examples

---
+++ tryexcept.py
@@ -0,0 +1,19 @@
+#try block lets you test a block of code for errors.
+#The except block lets you handle the error.
+#The else block lets you execute code when there is no error.
+#The finally block lets you execute code, regardless of the #result of the try- and except blocks.
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
+
+
+#Many exceptions:
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
---
+++ userinput.py
@@ -0,0 +1,6 @@
+#python always allows user input.
+#python uses input() method to take user input.
+#input() method returns a string.
+
+username = input("Enter username:")
+print("Username is: " + username)
