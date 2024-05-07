3907b67707818a335b96fa07544c41d49a929adb
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-07 11:35:56 +0000

    Explanation of strings

---
+++ string.py
@@ -0,0 +1,21 @@
+a = "Hello World"
+#everything inside single or double quotes is string
+print(a)
+#string is a collection of characters
+
+#can use quotes inside a string, as long as they #don't match the quotes surrounding the string
+
+b = "We are the so-called \"Vikings\" from the north."
+print(b)
+
+#can assign a multiline string to a variable by using three quotes
+
+c = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(c)
+#can access characters in a string by using its index
+
+#strings are arrays.
+
