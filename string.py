9b290cbdc2a7969a6875eaa9a825dd78556e9696
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-09 09:28:56 +0000

    Modifying strings

--- string.py
+++ string.py
@@ -62,4 +62,26 @@ print(x[:5])
 print(x[2:])
 #negative indexing
 #use negative indexes to start the slice from the end of the string
+print(x[-5:-2])
+#modify strings
+#The upper() method returns the string in upper case:
+a = "Hello, World!"
+print(a.upper())
+#The lower() method returns the string in lower case:
+a = "Hello, World!"
+print(a.lower())
+#The strip() method removes any whitespace from the beginning or the end:
+a = " Hello, World! "
+print(a.strip())
+#replace strings
+#The replace() method replaces a string with another string:
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#split strings
+#The split() method returns a list where the text between the specified separator becomes the list items.
+a = "Hello, World!"
+print(a.split(","))
+#string concatenation
+
+
 
