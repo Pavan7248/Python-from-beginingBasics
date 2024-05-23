454f4288c03769d59a978ed1c40883258e862807
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-23 15:53:49 +0000

    .

--- booleans.py
+++ booleans.py
@@ -4,3 +4,9 @@ print(10 > 11)
 print(23 == 24)
 print(12 < 13)
 print(10 >= 9)
+
+print(bool("Hello"))
+print(bool(15))
+print(bool(0))
+print(bool(None))
+# The bool() function allows you to evaluate any value, and give you True
