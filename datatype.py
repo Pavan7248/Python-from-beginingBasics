ea56ed23dbda0cee3abbb900bba725c42e37ba6d
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-05-30 13:19:05 +0000

    .

--- datatype.py
+++ datatype.py
@@ -13,10 +13,12 @@ print(False)
 print([1, 2, 3, 4, 5])
 # Tuples are a collection of items enclosed in parentheses separated by comma.
 print((1, 2, 3, 4, 5))
+
 # Dictionaries are a collection of key-value pairs enclosed in curly braces.
 print({"name": "Pavan", "age": 25})
 # None is a special value that represents the absence of a value.
 print(None)
+
 
 
 
